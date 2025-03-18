42a59c9c10f12d72cb45baac5fa4772547a185ec
smith-bimal <golokbihareebeemal@gmail.com>
2025-03-18 20:42:18 +0530

    Updated the auth to share json response instead of error

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
+  if (!req.header('Authorization')) return res.status(401).json({ message: 'No token, authorization denied' });
+
   const token = req.header('Authorization').replace('Bearer ', '');
   if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
 
