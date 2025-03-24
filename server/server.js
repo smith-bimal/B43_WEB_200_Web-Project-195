d18db6d673fa0819e313ac9214e86f5c408deab1
smith-bimal <golokbihareebeemal@gmail.com>
2025-03-24 09:41:19 +0530

    Fixed the bugs and added cors origin

--- server/server.js
+++ server/server.js
@@ -8,7 +8,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth.routes'));
